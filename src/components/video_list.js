cebf4c974c2a599ff4b06994fb7bc7420d7057cc
Nattaphol <rootncdev2015@gmail.com>
2018-02-02 14:28:16 +0700

    Finished2-25: List Item Keys
    - Need to provide key for each elements in the list 
    so React don't need to update the entire list when something have changed
    - An 'etag' is a property in video Object gathered from youtube.

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -3,7 +3,10 @@ import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
     const videoItems = props.videos.map((video) => {
-        return <VideoListItem video={video} />
+        // Need to provide key for each elements in the list 
+        // so React don't need to update the entire list when something have changed
+        // An 'etag' is a property in video Object gathered from youtube.
+        return <VideoListItem key={video.etag} video={video} />
     });
 
     return (
